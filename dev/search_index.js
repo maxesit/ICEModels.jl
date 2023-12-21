da2eaddbec7a3f69704840c9463d48b00220c0aa
Documenter.jl <documenter@juliadocs.github.io>
2023-12-21 19:15:09 +0000

    build based on d995c7a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ICEModels","category":"page"},{"location":"#ICEModels","page":"Home","title":"ICEModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ICEModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ICEModels]","category":"page"}]
+}
